0c531f0c22a94af4c924ed8230a6bd18806631ab
Sarmad Ajmal <24247576+sarmad-ajmal@users.noreply.github.com>
2023-06-05 10:50:27 +0400

    - initialize project - add vuetify

---
+++ plugins/vuetify.js
@@ -0,0 +1,16 @@
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/styles";
+
+export default defineNuxtPlugin(nuxtApp => {
+  const vuetify = createVuetify({
+    ssr: true,
+    components,
+    directives,
+  })
+
+  nuxtApp.vueApp.use(vuetify)
+})
