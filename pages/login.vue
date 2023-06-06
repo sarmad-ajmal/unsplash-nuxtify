0b209d9e96c47e62ce7ee7d57269caf508b804b4
Sarmad Ajmal <24247576+sarmad-ajmal@users.noreply.github.com>
2023-06-06 10:02:21 +0400

    add form rules to login page

--- pages/login.vue
+++ pages/login.vue
@@ -1,5 +1,14 @@
 <script setup>
 const layout = "full-center";
+
+const fromRules = {
+  required: (v) => !!v || "Required",
+};
+
+const login = () => {
+  if (this.$refs.loginForm.validate()) {
+  }
+};
 </script>
 <template>
   <NuxtLayout :name="layout">
@@ -16,51 +25,59 @@ const layout = "full-center";
           <h3 class="mb-0">Login</h3>
           <span>Welcome back.</span>
         </div>
-        <div>
-          <v-btn class="bg-blue-accent-3 w-100 py-6 d-flex" rounded="0" flat>
-            <v-icon left>mdi-facebook</v-icon>
-            <span class="ml-3 text-capitalize">Continue with Facebook</span>
-          </v-btn>
-          <div class="my-5 text-center text-caption">OR</div>
+        <v-form ref="loginForm">
           <div>
-            <label class="text-body-1 mb-2">Email</label>
-            <v-text-field
-              placeholder="Email"
-              variant="outlined"
-              dense
-              density="compact"
-              class="mb-5 mt-2"
-            ></v-text-field>
-          </div>
-          <div>
-            <div class="d-flex justify-space-between">
-              <label class="text-body-1 mb-2">Password</label>
-              <span class="text-grey text-decoration-underline pointer"
-                >Forgot your password?</span
-              >
+            <v-btn class="bg-blue-accent-3 w-100 py-6 d-flex" rounded="0" flat>
+              <v-icon left>mdi-facebook</v-icon>
+              <span class="ml-3 text-capitalize">Continue with Facebook</span>
+            </v-btn>
+            <div class="my-5 text-center text-caption">OR</div>
+            <div>
+              <label class="text-body-1 mb-2">Email</label>
+              <v-text-field
+                placeholder="Email"
+                variant="outlined"
+                dense
+                density="compact"
+                class="mb-5 mt-2"
+                :rules="[fromRules.required]"
+              ></v-text-field>
             </div>
-            <v-text-field
-              placeholder="Password"
-              density="compact"
-              variant="outlined"
-              dense
-              class="mb-0"
-            ></v-text-field>
-          </div>
-          <v-btn class="bg-black w-100 py-6 d-flex" rounded="0" flat href="/"
-            >Login</v-btn
-          >
-          <v-sheet
-            class="mt-4 d-flex justify-center align-center"
-            :height="90"
-            style="border-width: 2px"
-          >
-            <span>Don't have an account?</span>&nbsp;
-            <span class="text-grey text-decoration-underline pointer"
-              >Join Unsplash</span
+            <div>
+              <div class="d-flex justify-space-between">
+                <label class="text-body-1 mb-2">Password</label>
+                <span class="text-grey text-decoration-underline pointer"
+                  >Forgot your password?</span
+                >
+              </div>
+              <v-text-field
+                placeholder="Password"
+                density="compact"
+                variant="outlined"
+                dense
+                class="mb-0"
+              ></v-text-field>
+            </div>
+            <v-btn
+              class="bg-black w-100 py-6 d-flex"
+              rounded="0"
+              flat
+              @onClick="login()"
             >
-          </v-sheet>
-        </div>
+              Login</v-btn
+            >
+            <v-sheet
+              class="mt-4 d-flex justify-center align-center"
+              :height="90"
+              style="border-width: 2px"
+            >
+              <span>Don't have an account?</span>&nbsp;
+              <span class="text-grey text-decoration-underline pointer"
+                >Join Unsplash</span
+              >
+            </v-sheet>
+          </div>
+        </v-form>
       </v-col>
     </v-row>
   </NuxtLayout>
